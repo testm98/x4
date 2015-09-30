c24b26f4e725b73896dbcd4b388e0db2bd5e9c4e
Bruce Alan Martin <bam59cst112day@suffolk.li>
2015-09-30 13:22:17 -0400

    Update x4.java

--- x4.java
+++ x4.java
@@ -1,11 +1,46 @@
 //// Exercise 4:  click button to change background.
 
-//// SETUP:  size only.
+int r,g,b;
+float button1X=100, button1Y=100, button1W=80, button1H=40;
+
+//// SETUP:  size only.  Also set colors.
 void setup() {
   size( 640, 480 );
+  reset();
 }
-//// NEXT:  button only.
-void draw() {
-  background( 100,150,200 );
+void reset() {
+  r=  100;
+  g=  200;
+  b=  250;
 }
 
+
+//// NEXT:  button only.
+void draw() {
+  background( 100,200,250 );
+  showButton( button1X, button1Y, button1W, button1H );
+}
+// Draw the button.
+void showButton( float x, float y, float w, float h ) {
+  fill( 255,255,0 );
+  rect ( x,y, w,h );
+}
+
+//// HANDLERS:  keys & click
+void keyPressed() {
+  if (key == 'q') exit();
+}
+void mousePressed() {
+  if ( hit( mouseX,mouseY, 100,100, 50,50 ) ) {
+    background(0);
+  }
+}
+
+//// OTHER METHODS:  detect "hit"
+// Return true if "near"
+boolean hit( float x1, float y1, float x2, float y2, float w, float h ) {
+  boolean result;
+  // +++++ STUB ALWAYS RETURNS TRUE!
+  result=  true;
+  return result;
+}
