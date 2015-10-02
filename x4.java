82e7ff504365a9e239586603851ebc9b5612c1c5
testm98 <mtesta0727@gmail.com>
2015-10-02 11:39:42 -0400

    Update x4.java
    question regarding hit function

--- x4.java
+++ x4.java
@@ -1,63 +1,45 @@
-//// Exercise 4:  click button to change background.
-
-int r,g,b;
-float button1X=100, button1Y=100, button1W=80, button1H=40;
+///x4
+int r, g, b;
+String title= "Click Me!";
+float button1X=200, button1Y=200, button1W=80, button1H=80;
 int counter=0;
 
-//// SETUP:  size only.  Also set colors.
 void setup() {
-  size( 640, 480 );
+  background(200, 180, 150);
+  size( 400, 400);
   reset();
 }
-void reset() {
-  r=  100;
-  g=  200;
-  b=  250;
-}
 
-
-//// NEXT:  button only.
 void draw() {
-  background( r,g,b );
-  showButton( button1X, button1Y, button1W, button1H );
-  fill(100,0,0);
-  text( "Click me!", button1X+button1W/4, button1Y+button1H*2/3 );
-}
-// Draw the button.
-void showButton( float x, float y, float w, float h ) {
-  fill( 255,255,0 );
-  rect ( x,y, w,h );
+  button( button1X, button1Y, button1W, button1H);
 }
 
-//// HANDLERS:  keys & click
-void keyPressed() {
-  if (key == 'q') exit();
-  if (key == 'r') reset();
+void reset () {
+  r= 210;
+  g= 240;
+  b= 80;
 }
-void mousePressed() {
-  if ( hit( mouseX,mouseY, 100,100, 50,50 ) ) {
-    counter=  counter+1;
+
+void button( float x, float y, float w, float h ) {
+  fill(255, 0, 0);
+  ellipse( x, y, w, h);
+  fill(255);
+  text(title, button1X-25, button1Y+5);
+}
+
+/*void mousePressed() {
+  if ( hit (mouseX, mouseY, 200, 200, 60, 60 ) ) {
+    counter= counter+1;
     if (counter % 2 > 0) {
-      r=  255;
-      g=  50;
-      b=  0;
+      r= 160;
+      g= 200;
+      b= 80;
     } else {
       reset();
     }
   }
 }
+*/
 
-//// OTHER METHODS:  detect "hit"
-// Return true if "near"
-boolean hit( float x1, float y1, float x2, float y2, float w, float h ) {
-  boolean result;
 
-  // +++++ STUB ALWAYS RETURNS TRUE!
-  if ( abs(x1-x2) < w && abs(y1-y2)<h ) {
-    result=  true;
-  } else {
-    result=false;
-  }
 
-  return result;
-}
